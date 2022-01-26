e78be0ad66ded1ca41360c13f89e3b3a659b3b08
TacoJimmy <t5669016@ntut.org.tw>
2022-01-26 13:39:29 +0800

    Update airconditionctrl.py

--- airconditionctrl.py
+++ airconditionctrl.py
@@ -2,16 +2,22 @@ import time
 import json
 import time
 
-def read_mode():
-    with open('ACctrl.json') as ACCtrlMode:
+def read_ComCtrl():
+    with open('ComACctrl.json') as ACCtrlMode:
         ACOperate = json.load(ACCtrlMode)
         ACCtrlMode.close
         print (ACOperate)
     return ACOperate
 
-
+def read_SetCtrl():
+    with open('SetACctrl.json') as ACCtrlMode:
+        ACOperate = json.load(ACCtrlMode)
+        ACCtrlMode.close
+        print (ACOperate)
+    return ACOperate
 
 if __name__ == '__main__':
     while True:
-        read_mode()
+        read_ComCtrl()
+        read_SetCtrl()
         time.sleep(10)
